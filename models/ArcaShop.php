6c5f8017d230a68e439d26d185ad5a7e892c8d36
byHYONS <byhyons@gmail.com>
2024-06-26 16:20:56 +0200

    feat: add class product

--- models/ArcaShop.php
+++ models/ArcaShop.php
@@ -1,7 +1,34 @@
 <?php
-//? importo class
 
-//? classe padre ArcaShop:
+//? classe padre ArcaShop prodotti:
 class ArcaShop {
+
+
+    private string $title;
+    private int $price;
+    private string $image;
+
+    //* costruttore:
+    public function __construct($_title, $_price, $_image)
+    {
+        $this -> title = $_title;
+        $this -> price = $_price;
+        $this -> image = $_image;
+    }
+
+    //* titolo prodotto:
+    public function getTitle(): string {
+        return $this -> title;
+    }
+
+    //* prezzo prodotto:
+    public function getPrice(): string {
+        return $this -> price;
+    }
+
+    //* immagine prodotto:
+    public function getImage(): string {
+        return $this -> image;
+    }
     
 }
